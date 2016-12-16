904b28076e85190d090886bd044c507a49298b65
Jörn Schriewer <Joern.Schriewer@googlemail.com>
2016-12-16 03:15:45 +0100

    clean #2

--- BanjoBot/start.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Gibt den Benutzernamen aus, unter dem der PHP/HTTPD-Prozess läuft
-// (auf einem System in dem das Programm "whoami" im Ausführungspfad liegt)
-$output  echo exec('./BanjoBall');
-echo "<pre>$output</pre>";
-
-?>
